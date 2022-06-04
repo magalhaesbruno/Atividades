0a7a2c32f29f18a8735128dc1061c756cb9f5f88
magalhaesbruno <magalhaesbrunomb@gmail.com>
2022-06-03 22:50:36 -0300

    estudos

--- aula11/ex01.js
+++ aula11/ex01.js
@@ -1,5 +1,5 @@
-const prompt = require("prompt-sync")();
 const calculo = (n1, n2, operator) => {
   return `${n1} x ${n2} = ${n1 + operator + n2}`;
 };
-console.log(calculo(2, 3, "*"));
+const operador = +prompt("Digite a operação");
+console.log(calculo(2, 3, operador));
---
+++ aula11/teste.js
@@ -0,0 +1,6 @@
+const arrayColunaPc = ["A", "B", "C"];
+let pc = Math.floor(Math.random() * 3);
+let escolhapc = arrayColunaPc[pc];
+
+console.log(pc);
+console.log(escolhapc);
